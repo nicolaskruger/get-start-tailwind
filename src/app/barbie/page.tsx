b7df5aafadc991f5eceea7761318c8fc1fcd4052
nkruger <nicolas.nkruger@gmail.com>
2023-06-13 17:15:02 -0300

    style: :lipstick: barbi

---
+++ src/app/barbie/page.tsx
@@ -0,0 +1,11 @@
+import { BarbieButton } from "@/components/barbie-button";
+
+const Page = () => {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <BarbieButton />
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/components/barbie-button.tsx
@@ -0,0 +1,9 @@
+const BarbieButton = () => {
+  return (
+    <button className="first-letter:capitalize py-2 px-4 bg-pink-600 rounded-full hover:bg-pink-700 active:bg-pink-900 focus:ring-1 focus:ring-pink-300">
+      barbie
+    </button>
+  );
+};
+
+export { BarbieButton };
