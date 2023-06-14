69af4a21ffa00ea0e1f3c90964e588e838ab98f8
nkruger <nicolas.nkruger@gmail.com>
2023-06-14 17:27:51 -0300

    style: :lipstick: first line

---
+++ src/app/first-line/page.tsx
@@ -0,0 +1,20 @@
+import { CenterAllScreen } from "@/components/center-all-screen";
+
+const Page = () => {
+  return (
+    <CenterAllScreen>
+      <p
+        className="first-line:uppercase first-line:tracking-widest
+  first-letter:text-7xl first-letter:font-bold first-letter:text-white
+  first-letter:mr-3 first-letter:float-left
+"
+      >
+        Well, let me tell you something, funny boy. Y'know that little stamp,
+        the one that says "New York Public Library"? Well that may not mean
+        anything to you, but that means a lot to me. One whole hell of a lot.
+      </p>
+    </CenterAllScreen>
+  );
+};
+
+export default Page;
