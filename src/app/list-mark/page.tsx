1fc1eb4ec08624dd408e31b4f31f2a1e4951d25d
nkruger <nicolas.nkruger@gmail.com>
2023-06-14 17:20:29 -0300

    style: :lipstick: list

---
+++ src/app/list-mark/page.tsx
@@ -0,0 +1,16 @@
+import { CenterAllScreen } from "@/components/center-all-screen";
+
+const Page = () => {
+  return (
+    <CenterAllScreen>
+      <ul role="list" className="marker:text-sky-400 list-disc">
+        <li>mamimi</li>
+        <li>naota</li>
+        <li>kant</li>
+        <li>haruko</li>
+      </ul>
+    </CenterAllScreen>
+  );
+};
+
+export default Page;
