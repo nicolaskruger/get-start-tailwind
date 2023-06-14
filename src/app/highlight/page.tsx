ea9ab806b0f426ab664ea57df4790879a8eea3c7
nkruger <nicolas.nkruger@gmail.com>
2023-06-14 17:25:06 -0300

    style: :lipstick: highligth

---
+++ src/app/highlight/page.tsx
@@ -0,0 +1,19 @@
+import { CenterAllScreen } from "@/components/center-all-screen";
+
+const Highlight = () => {
+  return (
+    <CenterAllScreen>
+      <div className="selection:bg-fuchsia-300 selection:text-fuchsia-900">
+        <p>
+          So I started to walk into the water. I won't lie to you boys, I was
+          terrified. But I pressed on, and as I made my way past the breakers a
+          strange calm came over me. I don't know if it was divine intervention
+          or the kinship of all living things but I tell you Jerry at that
+          moment, I <em>was</em> a marine biologist.
+        </p>
+      </div>
+    </CenterAllScreen>
+  );
+};
+
+export default Highlight;
