413e3f8f0058b330df47306c766e18a28ded8b77
nkruger <nicolas.nkruger@gmail.com>
2023-06-19 14:58:28 -0300

    style: :lipstick: responsive design

---
+++ src/app/responsive-design/page.tsx
@@ -0,0 +1,11 @@
+import { CenterAllScreen } from "@/components/center-all-screen";
+
+const Page = () => {
+  return (
+    <CenterAllScreen>
+      <button className="after:content-['small'] sm:after:content-['small'] md:after:content-['mediun'] lg:after:content-['large']"></button>
+    </CenterAllScreen>
+  );
+};
+
+export default Page;
