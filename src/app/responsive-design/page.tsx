eb5e30d42c6d5a9f250ec227512fcd93bb09e85f
nkruger <nicolas.nkruger@gmail.com>
2023-06-19 14:59:09 -0300

    fix: :bug: typo error [medium]

--- src/app/responsive-design/page.tsx
+++ src/app/responsive-design/page.tsx
@@ -3,7 +3,7 @@ import { CenterAllScreen } from "@/components/center-all-screen";
 const Page = () => {
   return (
     <CenterAllScreen>
-      <button className="after:content-['small'] sm:after:content-['small'] md:after:content-['mediun'] lg:after:content-['large']"></button>
+      <button className="after:content-['small'] sm:after:content-['small'] md:after:content-['medium'] lg:after:content-['large']"></button>
     </CenterAllScreen>
   );
 };
