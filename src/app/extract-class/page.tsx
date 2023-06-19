e8306b7303f5de778451368d2ed7dc0b94a07eed
nkruger <nicolas.nkruger@gmail.com>
2023-06-19 14:37:43 -0300

    style: :lipstick: extends class

---
+++ src/app/extract-class/page.tsx
@@ -0,0 +1,12 @@
+import { CenterAllScreen } from "@/components/center-all-screen";
+
+const Page = () => {
+  return (
+    <CenterAllScreen>
+      <button className="bg-red-600 text-white rounded-full">not</button>
+      <button className="btn-primary">yes</button>
+    </CenterAllScreen>
+  );
+};
+
+export default Page;
